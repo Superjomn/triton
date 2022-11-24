ec232a74b6dc117039256be13dffd030d645203c
Superjomn <yanchunwei@outlook.com>
2022-11-24 19:40:04 +0800

    up

--- lib/codegen/selection/generator.cc
+++ lib/codegen/selection/generator.cc
@@ -2011,7 +2011,7 @@ void generator::visit_mma884(ir::dot_inst* C, ir::value *A, ir::value *B, ir::va
   {
     std::vector<Value*> args;
     for (auto& v : ptr_b){
-      auto newv = gep(v, i32(-512/16));
+      auto newv = gep(v, i32(-512));
       args.push_back(newv);
     }
     vprintf_array(gThreadId, args, "ptrBs", "%d", rewriter);
