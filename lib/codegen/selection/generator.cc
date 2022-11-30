881d0682f74343768c39a8ee33ba3cb6c7d28723
Superjomn <yanchunwei@outlook.com>
2022-11-30 10:56:10 +0800

    up

--- lib/codegen/selection/generator.cc
+++ lib/codegen/selection/generator.cc
@@ -2157,6 +2157,8 @@ void generator::visit_mma884(ir::dot_inst* C, ir::value *A, ir::value *B, ir::va
     vprintf("B t-%d pb,thePtrB,offset: %d,%d,%d\n", {gThreadId, pb, ptrb, offset}, rewriter);
     vprintf("pb t-%d %d\n", {gThreadId, pb}, rewriter);
     Value* hb =   load(bit_cast(pb, ptr_ty(vec_ty(i32_ty, vec_b/2), 3)));
+
+    vprintf("Boffsets t-%d %d %d %d\n", {gThreadId, i32_val(offidx), off_b[offidx], offset}, rewriter);
     // record lds that needs to be moved
     if (K == 0 && inc == 1 && is_prefetch)
       prefetch_latch_to_bb_[phiB->get_incoming_value(1)].push_back(hb);
